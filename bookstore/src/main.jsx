53fa48bc07e0951592b4a434a01cc54b312f5f4f
yash-mandali <yashmandali211@gmail.com>
2025-04-12 12:16:26 +0530

    bookstore commit

---
+++ bookstore/src/main.jsx
@@ -0,0 +1,42 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
+import Contact from './components/contact/Contact.jsx'
+import Settings from './components/Settings/Settings.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '',
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: 'contactus',
+        element: <Contact />,
+      },
+      {
+        path: 'settings',
+        element: <Settings />,
+      }
+    ]
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
