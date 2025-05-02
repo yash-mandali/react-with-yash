83aea68f5cf0df8fc7d80f5e55357e4722b1d9a5
yash-mandali <yashmandali211@gmail.com>
2025-05-02 10:59:39 +0530

    dark mode project

--- contextapi/src/components/Login.jsx
+++ contextapi/src/components/Login.jsx
@@ -1,25 +1,25 @@
-import React, { useContext , useState } from 'react';
+import React, { useContext, useState } from 'react';
 import UserContext from '../context/UserContext';
 function Login() {
 
     const [username, setusername] = useState('')
     const [password, setpassword] = useState('')
 
-    const {setUser} = useContext(UserContext)
+    const { setUser } = useContext(UserContext)
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        setUser({username,password})
-                }
+        setUser({ username, password })
+    }
     return (
         <>
             <div className="">
                 <h2>Login</h2>
                 <input type="text" placeholder='username' value={username} onChange={(e) => setusername(e.target.value)} />
                 <input type="text" placeholder='Password' value={password} onChange={(e) => setpassword(e.target.value)} />
-                 <button onClick={handleSubmit}>Submit</button>
+                <button onClick={handleSubmit}>Submit</button>
 
-          </div>
+            </div>
         </>
     );
 }
