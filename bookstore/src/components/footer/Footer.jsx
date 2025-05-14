6084558cb5919745784b0abb20ad623444c8ff52
yash-mandali <yashmandali211@gmail.com>
2025-05-14 11:10:34 +0530

    todoapp project

--- bookstore/src/components/footer/Footer.jsx
+++ bookstore/src/components/footer/Footer.jsx
@@ -56,7 +56,7 @@ export default function Footer() {
                                 </li>
                                 <li>
                                     <NavLink
-                                        to={"/setting"}
+                                        to={"/settings"}
                                         className={({ isActive }) =>
                                             `block mb-3 py-2 pr-4 pl-3 dark:text-gray-300 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                         }
---
+++ todoapp/src/contexts/Todocontext.js
@@ -0,0 +1,24 @@
+import { createContext, useContext } from "react";
+
+export const TodoContext = createContext({
+    todos: [
+        {
+            id: 1,
+            todo: "Todo 1 complate",
+            complated:true
+        }
+    ],
+
+    addTodo: (todo) => { },
+    updateTodo: (id, todo),
+    deleteTodo: (id) => { },
+    toggleComplete: (id) => { },
+    
+    
+}) 
+
+export const useTodo = () => {
+    return useContext(TodoContext)
+}
+
+export const Todoprovider = TodoContext.Provider
